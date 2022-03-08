696a220b3c3b62a2c987bb08a59e4ef7ddfd09a2
KuskIV <madshkusk@gmail.com>
2022-03-08 21:57:15 +0100

    realtime

---
+++ SaxoRealTimeData/SaxoRealTimeData/SaxoWorker.cs
@@ -0,0 +1,34 @@
+﻿using Serilog;
+
+namespace SaxoRealTimeData
+{
+    public class SaxoWorker : BackgroundService
+    {
+        private readonly Serilog.ILogger _logger;
+
+        public SaxoWorker(Serilog.ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                try
+                {
+                    _logger.Warning("horse");
+                }
+                catch (Exception exception)
+                {
+
+                    throw;
+                }
+                
+                Task.Delay(10000, stoppingToken);
+            }
+         
+            return Task.CompletedTask;
+        }
+    }
+}
