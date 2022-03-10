aa8f8e77a4f3cbcb1a8574862b2a2b05639a8446
Jeppe <jeppe@bandsholm.com>
2022-03-10 11:33:40 +0100

    test update

--- Forcasting_Models/Database access/DatasetAccess.py
+++ Forcasting_Models/Database access/DatasetAccess.py
@@ -1,3 +1,4 @@
+from urllib3 import Retry
 from DatabaseConnection import DatabaseConnection
 class DatasetAccess:
     def __init__(self):
@@ -9,8 +10,12 @@ class DatasetAccess:
     
     def getStockFrom(self, StockSymbol):
         company = self.conn.query("SELECT * FROM dataset WHERE symbol = '" + StockSymbol + "'")
+        return company
 DatasetAccess = DatasetAccess()
 companys = DatasetAccess.getAllCompanyes()
     
 print("selecting the first stock")
-print(companys[0].symbol)
+print(companys[0][9])
+
+company = DatasetAccess.getStockFrom(companys[0][9])
+print(company)
