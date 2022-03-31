25b46569c312bf1e1e4ac88617a1e9cd5fd7c098
GreatestHoro <rhoro18@student.aau.dk>
2022-03-31 13:53:14 +0200

    i am superior

---
+++ Forcasting_Models/FbProphet/fbprophet.py
@@ -0,0 +1,61 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import psycopg2 as pg
+import datetime
+from fbprophet import Prophet
+from fbprophet.diagnostics import cross_validation, performance_metrics
+from fbprophet.plot import plot_cross_validation_metric
+import time
+# from sklearn.metrics import mean_squared_error
+from matplotlib import dates as mdates
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+import statsmodels.api as sm
+from statsmodels.tsa.seasonal import seasonal_decompose
+from pandas.plotting import register_matplotlib_converters
+register_matplotlib_converters()
+plt.style.use("ggplot")
+
+import utils.settings_utils as settings
+import utils.DatasetAccess as db_access
+import utils.preprocess as preprocess
+import utils.prophet_experiment as exp
+
+
+def model_fit(training_set, mcmc_samples=300, yearly_seasonality=False, weekly_seasonality=False, daily_seasonality=False, seasonality_mode='multiplicative'):
+    model = Prophet(mcmc_samples=mcmc_samples,
+                    seasonality_mode=seasonality_mode,
+                    yearly_seasonality=yearly_seasonality, 
+                    weekly_seasonality=weekly_seasonality,
+                    daily_seasonality=daily_seasonality)
+    
+    model.fit(training_set)
+    
+    return model
+
+
+def get_cross_validation(model, horizon, initial, cutoff):
+    return cross_validation(model, horizon=horizon, initial=initial, cutoff=cutoff)
+
+
+def get_performance_metrics(df):
+    return performance_metrics(df)
+
+def get_future_df(model, period, freq, include_history=False):
+    return model.make_future_dataframe(periods=period, freq=freq, include_history=include_history)
+
+def make_prediction(model, df):
+    return model.predict(df)
+
+def plot_cross_validation(df):
+    df.plot(x='ds', y=['y', 'yhat'], figsize=(12, 8), grid=True, title='Forecast', legend=True, fontsize=12, linewidth=2)
+    plt.show()
+
+def plot_forecast(model, forecast, test_data):
+    model.plot(forecast)
+    plt.plot(test_data.ds, test_data.y, '.', color='#ff3333', alpha=0.6)
+    model.plot_components(forecast)
+    plt.show()
--- Forcasting_Models/utils/arguments.py
+++ Forcasting_Models/utils/arguments.py
@@ -7,6 +7,15 @@ def get_arguments():
     parser.add_argument("--primarycategory", nargs="+", required=False)
     parser.add_argument("--secondarycategory", nargs="+", required=False)
     parser.add_argument("--companyid", nargs="+", required=False, default=[15515])
+    parser.add_argument("--timeunit", required=False, default='H')
+    parser.add_argument("--time", required=False, default='0001-01-01 00:00:00')
+    parser.add_argument("--seasonality_mode", required=False, default='additive')
+    parser.add_argument("--yearly_seasonality", required=False, default=False)
+    parser.add_argument("--weekly_seasonality", required=False, default=False)
+    parser.add_argument("--daily_seasonality", required=False, default=False)
+    parser.add_argument("--include_history", required=False, default=False)
+    parser.add_argument("--predict_periods", required=False, default=10)
+
 
     args = parser.parse_args()
 
